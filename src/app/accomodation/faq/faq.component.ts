f0b7145a18e1ec576449a825d453ea52814b36fd
Aditya Rana <official.adityarana01@gmail.com>
2020-08-21 02:18:31 +0530

    Deleted redundant directories and created single component for accomodation. Updated routes in accomodation-routing

--- src/app/accomodation/faq/faq.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
-})
-export class faqComponent {
-}
