013da89206f5ec02c131a66f94c6a4fab8203660
Aditya Rana <official.adityarana01@gmail.com>
2020-08-23 14:55:32 +0530

    Removed stray curly braces and code

--- src/app/accomodation/accomodation.module.ts
+++ src/app/accomodation/accomodation.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AccomodationRoutingModule } from "./accomodation-routing.module"
-import { AccomodationComponent } from "./accomodation.component"
-import {  }
+import { AccomodationRoutingModule } from "./accomodation-routing.module";
+import { AccomodationComponent } from "./accomodation.component";
 
 @NgModule({
   declarations: [
