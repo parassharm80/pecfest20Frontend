5872c1df9869d668f6b7c060aa2f5752958824a1
Aditya Rana <official.adityarana01@gmail.com>
2020-08-25 21:03:19 +0530

    Refactored for standard practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatCheckboxModule, } from "@angular/material/checkbox";
-import {MatToolbarModule} from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
