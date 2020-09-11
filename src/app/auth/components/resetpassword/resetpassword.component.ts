a0058771b196f19bb42cef378ab50db85d68eb5b
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-11 17:54:55 +0530

    Reset password instructions

--- src/app/auth/components/resetpassword/resetpassword.component.ts
+++ src/app/auth/components/resetpassword/resetpassword.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-resetpassword',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetpasswordComponent implements OnInit {
 
-  constructor() { }
+  email_id=new FormControl('',Validators.email);
+  private url="http://localhost:8080/generate-verification-code";
+  param=new HttpParams();
+  submitted: boolean=false;
+  message=null;
+  successMessage=null;
+  constructor(private fb:FormBuilder,private http:HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.submitted=true;
+    this.message=null;
+    this.successMessage=null;
+    if(this.email_id.valid)
+      this.http.get(this.url,{params:this.param.append("emailId",this.email_id.value)})
+        .subscribe(response=>{
+          if(response["http_status"]!="OK")
+            this.message=response["status_message"];
+          else
+            this.successMessage="Check your email for password reset instructions."
+        });
+  }
 }
