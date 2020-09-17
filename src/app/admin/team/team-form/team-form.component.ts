6dccf454baaffe0ee28ab7b1b112b7cdb50d363e
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-17 15:31:49 +0530

    making them inline

--- src/app/admin/team/team-form/team-form.component.ts
+++ src/app/admin/team/team-form/team-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-team-form',
   templateUrl: './team-form.component.html',
   styleUrls: ['./team-form.component.css']
 })
+@Injectable({ providedIn: 'root'})
 export class TeamFormComponent implements OnInit {
 
   constructor() { }
