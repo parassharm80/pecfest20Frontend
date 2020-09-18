a7db09b6581193e776f9bf2f28b4aed67db374d9
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-18 13:38:17 +0530

    Team form component

--- src/app/admin/team/team-form/team-form.component.ts
+++ src/app/admin/team/team-form/team-form.component.ts
@@ -1,5 +1,11 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {TeamFormService} from './team-form.service';
+import {FormGroup} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatDialogRef} from '@angular/material/dialog';
+import {AdminService} from '../../admin.service';
+import {ProdEnvService} from '../../../prod-env.service';
+import {TeamAdminService} from '../team-admin/team-admin.service';
 
 @Component({
   selector: 'app-team-form',
@@ -9,8 +15,9 @@ import {TeamFormService} from './team-form.service';
 @Injectable({ providedIn: 'root'})
 export class TeamFormComponent implements OnInit {
 
-  constructor(private formService:TeamFormService) { }
-
+  constructor(private formService:TeamFormService,private snackBar: MatSnackBar,
+              private dialogRef: MatDialogRef<TeamFormComponent>,private adminService:TeamAdminService,public  prodEnvService:ProdEnvService) { }
+  public form: FormGroup = this.formService.form;
   ngOnInit(): void {
   }
 
@@ -23,5 +30,17 @@ export class TeamFormComponent implements OnInit {
       leader_pec_fest_id:'',
       member_pec_fest_id_list: '',
     });
+  }
+
+  onClose() {
+
+  }
+
+  newSubmit() {
+
+  }
+
+  onClear() {
+
   }
 }
