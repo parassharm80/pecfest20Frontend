6b584681b8f80d28170e74db9f5e39d3477ee3e8
himanshugaur17 <himanshugaur1215@gmail.com>
2020-09-18 13:23:24 +0530

    Team form service

--- src/app/admin/team/team-form/team-form.service.ts
+++ src/app/admin/team/team-form/team-form.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamFormService {
-
-  constructor() { }
+  public form: FormGroup = new FormGroup({
+    $key: new FormControl(null),
+    event_name: new FormControl('', Validators.required),
+    event_id: new FormControl('', Validators.required),
+    organizing_club: new FormControl('', Validators.required),
+    team_name: new FormControl('', Validators.required),
+    leader_pec_fest_id: new FormControl('', Validators.required),
+    member_pec_fest_id_list: new FormControl('', Validators.required),
+  })
 }
